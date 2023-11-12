c288d6d7529c99917b0029e5b53d98868348132a
Henry <19509472+henryliu5@users.noreply.github.com>
2023-11-12 02:09:37 -0600

    cp tmux.conf

--- setup.sh
+++ setup.sh
@@ -4,6 +4,7 @@ git clone https://github.com/henryliu5/kickstart.nvim.git "${XDG_CONFIG_HOME:-$H
 
 # clone tmux.conf
 git clone git@github.com:henryliu5/tmux.git 
+cp tmux/.tmux.conf ~/.tmux.conf
 
 # Install TPM
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
