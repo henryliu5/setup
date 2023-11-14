c68cbe33e974292d867a447611db1bd7b1ec5252
Henry Liu <19509472+henryliu5@users.noreply.github.com>
2023-11-13 22:03:33 -0600

    add lazygit

--- install.sh
+++ install.sh
@@ -1,3 +1,12 @@
+# Lazygit
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf lazygit.tar.gz lazygit
+sudo install lazygit /usr/local/bin
+
+rm lazygit.tar.gz
+rm lazygit
+
 # Mason needs zip/unzip
 sudo apt-get install -y zip unzip
 sudo snap install --beta nvim --classic
